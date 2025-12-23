67987af08534365f6a45018987ec42c0919d77d2
SpaceboiX <61667451+SpaceboiX@users.noreply.github.com>
2025-12-23 20:37:07 +0000

    Add files via upload

---
+++ assets/js/login-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDPezCM3sMDT46v_dMUtsNegFNxc801Jr0",
+  authDomain: "craftinite-gb.firebaseapp.com",
+  projectId: "craftinite-gb",
+  storageBucket: "craftinite-gb.firebasestorage.app",
+  messagingSenderId: "402693325036",
+  appId: "1:402693325036:web:7a2be506c8c162ac26e5c9",
+  measurementId: "G-R42Q7FZDQT"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
---
+++ assets/js/product-data.js
@@ -0,0 +1,30 @@
+// products-data.js
+const products = [
+  {
+    id: "christmas_card",
+    name: "Christmas Cutout Card",
+    image: "assets/img/products/christmas_card.webp",
+    description: "A warming Christmas card.",
+    price: 3,
+    type: "Cards",
+    tags: ["minimalist", "christmas", "card"]
+  },
+  {
+    id: "lovemark",
+    name: "Lovemark",
+    image: "assets/img/products/lovemark.webp",
+    description: "Minimalist bookmark with a loving style.",
+    price: 1,
+    type: "Bookmarks",
+    tags: ["bookmark", "minimalist", "gift"]
+  },
+  {
+    id: "craftinite_mug",
+    name: "Craftinite Mug",
+    image: "assets/img/products/craftinite_mug.webp",
+    description: "Mug holding the Craftinite logo.",
+    price: 12.50,
+    type: "Mugs",
+    tags: ["mug", "craftinite", "ceramic", "gift", "cosmic"]
+  }
+];
