3a3f674a9ce52f1e6de56b5515861773aaa199b9
SpaceboiX <61667451+SpaceboiX@users.noreply.github.com>
2026-03-05 13:15:14 +0000

    Delete assets/js/login-google.js

--- assets/js/login-google.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("google-login").addEventListener("click", () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  auth.signInWithPopup(provider)
-    .then(() => window.location.href = "index.html")
-    .catch(err => alert(err.message));
-});
