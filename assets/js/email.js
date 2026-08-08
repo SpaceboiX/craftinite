42f39a9733bbf1cfdcbde5d9d69dd5c60e7a2b2d
SpaceboiX <61667451+SpaceboiX@users.noreply.github.com>
2026-08-08 02:19:03 +0100

    Add files via upload

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (typeof emailjs !== "undefined") {
-        emailjs.init("QrtwE4LJqNzj1yHjz");
+        emailjs.init({
+            publicKey: "QrtwE4LJqNzj1yHjz"
+        });
         console.log("EmailJS initialized");
     } else {
         console.error("EmailJS SDK failed to load");
