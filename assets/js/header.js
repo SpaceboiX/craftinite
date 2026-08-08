e79f5300fdca1461d7aa45a3cc2b9638b1e7bfd1
SpaceboiX <61667451+SpaceboiX@users.noreply.github.com>
2026-08-08 01:47:50 +0100

    Delete assets directory

--- assets/js/header.js
+++
@@ -1,6 +0,0 @@
-// Load header only — no login, no profile, no auth, no cart
-fetch("header.html?v=3")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("header-placeholder").innerHTML = html;
-  });
